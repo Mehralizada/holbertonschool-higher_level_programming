1820ed14756b36d9012a8991573f5a3c3daef979
Mehralizada <gulanemehrelizade97@gmail.com>
2024-05-20 02:44:00 +0000

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
      names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    for name in sorted(names):
+        if not name.startswith("__"):
             print(name)
