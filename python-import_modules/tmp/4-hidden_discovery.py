b2ae4f217a50ffdf09a0e7465fe74ab052ce0c34
Mehralizada <gulanemehrelizade97@gmail.com>
2024-05-20 17:32:12 +0000

    updated

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -1,18 +1,8 @@
-#!/usr/bin/python3.8
-import importlib.util
-import sys
-
-def main():
-    module_name = "hidden_4"
-    file_path = "/tmp/hidden_4.pyc"
-
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    
-    names = [name for name in dir(module) if not name.startswith("__")]
-    for name in sorted(names):
-        print(name)
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    main()
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
