87a8693a205ac15d42e137d58c9f4f67df14eee7
Mehralizada <gulanemehrelizade97@gmail.com>
2024-05-24 03:21:48 +0000

    updated

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,7 @@
 
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or 
-            roman_string is None):
+    if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
     roman_dict = {
@@ -21,8 +20,7 @@ def roman_to_int(roman_string):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and 
-        roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+        if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
             num += roman_dict[roman_string[i]] * -1
         else:
             num += roman_dict[roman_string[i]]
