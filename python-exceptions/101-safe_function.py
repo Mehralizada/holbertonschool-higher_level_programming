e13110e841dd8fa66b4503af9709a0d24e6ad042
Mehralizada <gulanemehrelizade97@gmail.com>
2024-05-24 12:29:29 +0000

    101-safe_function.py

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 101-safe_function.py
-# Brennan D Baraban <375@holbertonschool.com>
+
 
 import sys
 
@@ -18,7 +17,8 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return (None)
+
