b659022d88c7822f92d032cbad337e34f7b52d3a
Mehralizada <gulanemehrelizade97@gmail.com>
2024-06-07 00:52:16 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python3
+"""
+This module represents retrieving a
+dictionary representation of a Student instance
+"""
+
+
 class Student:
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
-# Example usage:
-if __name__ == "__main__":
-    student_1 = Student("John", "Doe", 23)
-    student_2 = Student("Bob", "Dylan", 27)
+        if not isinstance(attrs, list):
+            return self.__dict__
 
-    j_student_1 = student_1.to_json()
-    j_student_2 = student_2.to_json(['first_name', 'age'])
-    j_student_3 = student_2.to_json(['middle_name', 'age'])  # 'middle_name' is not an attribute
+        filtered_dict = {}
 
-    print(j_student_1)  # {'first_name': 'John', 'last_name': 'Doe', 'age': 23}
-    print(j_student_2)  # {'first_name': 'Bob', 'age': 27}
-    print(j_student_3)  # {'age': 27}
+        for attr in attrs:
+            if isinstance(attr, str) and attr in self.__dict__:
+                filtered_dict[attr] = self.__dict__[attr]
+
+        return filtered_dict
