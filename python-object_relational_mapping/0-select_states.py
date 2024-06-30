8de6d6eec02da5146f5364689189289805cb0348
Mehralizada <gulanemehrelizade97@gmail.com>
2024-06-30 11:49:56 +0000

    file updated

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Command line arguments: username, password, database name
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # SQL query to retrieve all states sorted by id
-    sql_query = "SELECT * FROM states ORDER BY id ASC"
-
-    try:
-        # Execute the SQL command
-        cursor.execute(sql_query)
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-        # Print each row
-        for row in results:
-            print(row)
-    except MySQLdb.Error as e:
-        print(f"Error executing the query: {e}")
-
-    # Disconnect from server
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
