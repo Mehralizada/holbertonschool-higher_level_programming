1e1f58e80753a7f85e31e18206753a82695883e5
Mehralizada <gulanemehrelizade97@gmail.com>
2024-07-29 09:32:05 +0000

    file created

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,8 @@
--- Creates the user user_0d_1 with all privileges.
--- Creates the user user_0d_1 with all privileges.
+-- Script that creates the MySQL server user user_0d_1
+-- User_0d_1 should have all privileges on your MySQL server
 
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
